6ac99d93e9a5da9917df0a1b0440cc7f7ddd90f7
Dijalma Silva <dijalmacz@gmail.com>
2025-03-24 17:22:55 -0300

    Update messages.ts
    Atualizando mensagem do novo convertido

--- app/constants/messages.ts
+++ app/constants/messages.ts
@@ -11,13 +11,17 @@ const Messages: Record<
 > = {
 	"Novo convertido": (name, welcoming) => `Graça e paz, *${name}*!
 - Me chamo *${welcoming}*, sou do departamento de Acolhimento da Igreja Sal da Terra, que você visitou e onde fez sua confissão de fé em Jesus Cristo *ontem*!
-Gostaria de saber como foi a sua experiência na nossa igreja e te dar os parabéns pela confissão de fé em Cristo Jesus como seu único Senhor e Salvador. Lembrando que temos culto todas as Quintas-feiras, às 19h30, e aos Domingos, às 19h. Além disso, temos o Connect Jovem, de 15 a 17 anos, às 18h e o Connect+, 18 ou mais, às 20hrs nas sextas-feiras.
-E por fim, o Connect Teens, de 10 a 14 anos, aos sábados, às 16h!
-Desde já, fico muito feliz em estabelecermos uma conversa de fé cristã! 🙏`,
+Gostaria de te dar os parabéns pela confissão de fé em Cristo Jesus como seu único Senhor e Salvador e dizer que estamos todos muito felizes por esse seu passo de fé. Lembrando que temos culto todas as Quintas-feiras, às 19h30, e aos Domingos, às 19h. Além disso, temos o Connect Jovem, de 15 a 17 anos, às 18h e o Connect+, de 18 anos acima, às 20hrs, ambos nas sextas-feiras.
+E por fim, o Connect Teens, de 10 a 14 anos, todos os sábados, às 16h!
+Estou te enviando o link para entrar no grupo do Whatsapp da Sal da Terra, dessa família a qual você também já faz parte. Por lá recebemos avisos a respeito da programação da igreja, bem como estabelecemos momentos de comunhão com os irmãos. Será de grande valia se puder fazer parte dele.
+
+https://chat.whatsapp.com/Ic3Od5mN1Nh8DtZYEPIOWu
+
+Mais uma vez, os nossos parabéns, qualquer dúvida pode falar comigo. Até breve! 🙏`,
 	Visitante: (name, welcoming) => `Olá, *${name}*!
 - Me chamo *${welcoming}*, sou do Departamento de Acolhimento da Igreja Sal da Terra, que você visitou *ontem*!
-Gostaria de saber como foi a sua experiência como visitante, no que podemos melhorar para te receber melhor e te convidar a retornar à nossa Igreja. Lembrando que temos culto todas as Quintas-feiras, às 19h30, e aos Domingos, às 19h. Além disso, temos o Connect Jovem, de 15 a 17 anos, às 18h e o Connect+, 18 ou mais, às 20hrs nas sextas-feiras.
-E por fim, o Connect Teens, de 10 a 14 anos, aos sábados, às 16h!
+Gostaria de saber como foi a sua experiência como visitante, no que podemos melhorar para te receber melhor e te convidar a retornar à nossa Igreja. Lembrando que temos culto todas as Quintas-feiras, às 19h30, e aos Domingos, às 19h. Além disso, temos o Connect Jovem, de 15 a 17 anos, às 18h e o Connect+, de 18 anos acima, às 20hrs, ambos nas sextas-feiras.
+E por fim, o Connect Teens, de 10 a 14 anos, todos os sábados, às 16h!
 Desde já, fico muito contente em estabelecermos uma conversa de fé cristã! 🙏`,
 };
 
